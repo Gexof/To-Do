f81891ed96940b62e1dd3f75b8acd616051e258b
Mohamed Ashraf <gexof01@gmail.com>
2024-01-17 21:29:48 +0200

    Done The Functionallyt

--- main.js
+++ main.js
@@ -4,151 +4,19 @@ let todoDiv = document.getElementById("todo-div");
 let progressDiv = document.getElementById("progress-div");
 let doneDiv = document.getElementById("done-div");
 
-// let todoList = [];
-// let progressList = [];
-// let doneList = [];
-
-// getTodo();
-// getProg();
-// getDone();
-
-// // Drag & Drop FUNCTIONS
-// function drag(event) {
-//   event.dataTransfer.setData("text", event.target.id);
-//   event.dataTransfer.setData("par-id", event.target.parentElement.id);
-// }
-
-// function allowDrop(event) {
-//   event.preventDefault();
-// }
-
-// function drop(event) {
-//   event.preventDefault();
-//   let id = event.dataTransfer.getData("text");
-//   let parId = event.dataTransfer.getData("par-id");
-//   let draggedItem = document.getElementById(id);
-
-//   let uniqueId = "todo-" + new Date().getTime();
-
-//   event.target.appendChild(draggedItem);
-
-//   // event.target.id to get target div  //      parId to get id of the parent
-
-//   if (event.target.id === "progress-div") {
-//     if (parId === "todo-div") {
-//       remTodoFromList(todoList, id);
-//       addTodoToList(progressList, draggedItem.innerText);
-//       getTodo();
-//       getProg();
-//     } else if (parId === "done-div") {
-//       remTodoFromList(doneList, id);
-//       addTodoToList(progressList, draggedItem.innerText);
-//       getProg();
-//       getDone();
-//     }
-//   } else if (event.target.id === "done-div") {
-//     if (parId === "todo-div") {
-//       remTodoFromList(todoList, id);
-//       addTodoToList(doneList, draggedItem.innerText);
-//       getTodo();
-//       getDone();
-//     } else if (parId === "progress-div") {
-//       remTodoFromList(progressList, id);
-//       addTodoToList(doneList, draggedItem.innerText);
-//       getProg();
-//       getDone();
-//     }
-//   } else if (event.target.id === "todo-div") {
-//     if (parId === "progress-div") {
-//       remTodoFromList(progressList, id);
-//       addTodoToList(todoList, draggedItem.innerText);
-
-//       getTodo();
-//       getProg();
-//     } else if (parId === "done-div") {
-//       remTodoFromList(doneList, id);
-//       addTodoToList(todoList, draggedItem.innerText);
-//       getTodo();
-//       getDone();
-//     }
-//   }
-
-//   console.log(todoList);
-//   console.log(progressList);
-//   console.log(doneList);
-// }
-
-// function addTodo() {
-//   todoList.push(todoText.value);
-//   getTodo();
-// }
-
-// function remTodoFromList(list, id) {
-//   list.splice(id, 1);
-// }
-
-// function addTodoToList(list, item) {
-//   list.push(item);
-// }
-
-// function getTodo() {
-//   let todos = ``;
-//   for (let i = 0; i < todoList.length; i++) {
-//     todos += `<div class= "todo-r" id= "${i}"  draggable="true" ondragstart="drag(event)">
-//     <h5 id="todo${i}">${todoList[i]}</h5>
-//     </div>`;
-//   }
-
-//   todoDiv.innerHTML = todos;
-// }
-
-// function getProg() {
-//   let todos = ``;
-//   for (let i = 0; i < progressList.length; i++) {
-//     todos += `<div class= "todo-r" id= "${i}"  draggable="true" ondragstart="drag(event)">
-//     <h5 id="todo${i}">${progressList[i]}</h5>
-//     </div>`;
-//   }
-
-//   progressDiv.innerHTML = todos;
-// }
-
-// function getDone() {
-//   let todos = ``;
-//   for (let i = 0; i < doneList.length; i++) {
-//     todos += `<div class= "todo-r" id= "${i}"  draggable="true" ondragstart="drag(event)">
-//     <h5 id="todo${i}">${doneList[i]}</h5>
-//     </div>`;
-//   }
-
-//   doneDiv.innerHTML = todos;
-// }
-
-// Arrays to store todos in different states
-
-let todoList = [];
-let progressList = [];
-let doneList = [];
-
-// if (localStorage.getItem("Todo") != null) {
-//   todoList = JSON.parse(localStorage.getItem("Todo"));
-// } else {
-//   todoList = [];
-// }
-
-checkForIntializeItems("Todo", todoList);
-
-function checkForIntializeItems(storgeName, list) {
-  if (localStorage.getItem(storgeName) != null) {
-    list = JSON.parse(localStorage.getItem(storgeName));
-  } else {
-    list = [];
-  }
+function initializeFromLocalStorage(storageName) {
+  const storedData = localStorage.getItem(storageName);
+  return storedData ? JSON.parse(storedData) : [];
 }
+
+let todoList = initializeFromLocalStorage("Todo");
+let progressList = initializeFromLocalStorage("Progress");
+let doneList = initializeFromLocalStorage("Done");
 
 getTodo();
 getProg();
 getDone();
+
 // Drag & Drop FUNCTIONS
 function drag(event) {
   event.dataTransfer.setData("text", event.target.id);
@@ -172,45 +40,57 @@ function drop(event) {
   // event.target.id to get target div  // parId to get id of the parent
   if (event.target.id === "progress-div") {
     if (parId === "todo-div") {
-      remTodoFromList(todoList, id);
-      addTodoToList(progressList, {
-        id: uniqueId,
-        text: draggedItem.innerText,
-      });
-      getTodo();
-      getProg();
+      remTodoFromList(todoList, id, "Todo");
+      addTodoToList(
+        progressList,
+        {
+          id: uniqueId,
+          text: draggedItem.innerText,
+        },
+        "Progress"
+      );
     } else if (parId === "done-div") {
-      remTodoFromList(doneList, id);
-      addTodoToList(progressList, {
-        id: uniqueId,
-        text: draggedItem.innerText,
-      });
-      getProg();
-      getDone();
+      remTodoFromList(doneList, id, "Done");
+      addTodoToList(
+        progressList,
+        {
+          id: uniqueId,
+          text: draggedItem.innerText,
+        },
+        "Progress"
+      );
     }
   } else if (event.target.id === "done-div") {
     if (parId === "todo-div") {
-      remTodoFromList(todoList, id);
-      addTodoToList(doneList, { id: uniqueId, text: draggedItem.innerText });
-      getTodo();
-      getDone();
+      remTodoFromList(todoList, id, "Todo");
+      addTodoToList(
+        doneList,
+        { id: uniqueId, text: draggedItem.innerText },
+        "Done"
+      );
     } else if (parId === "progress-div") {
-      remTodoFromList(progressList, id);
-      addTodoToList(doneList, { id: uniqueId, text: draggedItem.innerText });
-      getProg();
-      getDone();
+      remTodoFromList(progressList, id, "Progress");
+      addTodoToList(
+        doneList,
+        { id: uniqueId, text: draggedItem.innerText },
+        "Done"
+      );
     }
   } else if (event.target.id === "todo-div") {
     if (parId === "progress-div") {
-      remTodoFromList(progressList, id);
-      addTodoToList(todoList, { id: uniqueId, text: draggedItem.innerText });
-      getTodo();
-      getProg();
+      remTodoFromList(progressList, id, "Progress");
+      addTodoToList(
+        todoList,
+        { id: uniqueId, text: draggedItem.innerText },
+        "Todo"
+      );
     } else if (parId === "done-div") {
-      remTodoFromList(doneList, id);
-      addTodoToList(todoList, { id: uniqueId, text: draggedItem.innerText });
-      getTodo();
-      getDone();
+      remTodoFromList(doneList, id, "Done");
+      addTodoToList(
+        todoList,
+        { id: uniqueId, text: draggedItem.innerText },
+        "Todo"
+      );
     }
   }
 
@@ -221,21 +101,32 @@ function drop(event) {
 
 function addTodo() {
   let uniqueId = "todo-" + new Date().getTime();
-  let newTodo = { id: uniqueId, text: todoText.value };
+  let newTodoText = todoText.value.trim(); // Trim the text content
+  let newTodo = { id: uniqueId, text: newTodoText };
   todoList.push(newTodo);
   localStorage.setItem("Todo", JSON.stringify(todoList));
   getTodo();
 }
 
-function remTodoFromList(list, id) {
+function remTodoFromList(list, id, storageName) {
   let index = list.findIndex((todo) => todo.id === id);
   if (index !== -1) {
     list.splice(index, 1);
+    localStorage.setItem(storageName, JSON.stringify(list));
+    getTodo();
+    getProg();
+    getDone();
   }
 }
 
-function addTodoToList(list, item) {
-  list.push(item);
+function addTodoToList(list, item, storageName) {
+  let trimmedText = item.text.trim(); // Trim the text content
+  let newItem = { id: item.id, text: trimmedText };
+  list.push(newItem);
+  localStorage.setItem(storageName, JSON.stringify(list));
+  getTodo();
+  getProg();
+  getDone();
 }
 
 function getTodo() {
